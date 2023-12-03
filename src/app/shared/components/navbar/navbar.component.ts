11b847ccc493d856192a2852555f5d25fc82640d
Anjo Tadena <tadena.anjo@gmail.com>
2023-12-03 10:33:36 +0800

    Change component prefix

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-navbar',
+  selector: 'sc-navbar',
   standalone: true,
   imports: [],
   templateUrl: './navbar.component.html',

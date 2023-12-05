2270a4a6d3b49e831689e481635eccf5fec2c384
Anjo Tadena <tadena.anjo@gmail.com>
2023-12-05 09:27:47 +0800

    Add product filters

--- src/app/shop/shop.component.ts
+++ src/app/shop/shop.component.ts
@@ -1,19 +1,26 @@
-import { Component, OnInit, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit, inject, signal } from '@angular/core';
 import { ShopService } from './shop.service';
 import { toObservable, toSignal } from '@angular/core/rxjs-interop';
 import { ProductItemComponent } from "./components/product-item/product-item.component";
+import { ActivatedRoute, RouterModule } from '@angular/router';
+import { map } from 'rxjs';
 
 @Component({
     selector: 'sc-shop',
     standalone: true,
     templateUrl: './shop.component.html',
     styleUrl: './shop.component.scss',
-    imports: [ProductItemComponent]
+    imports: [CommonModule, RouterModule, ProductItemComponent]
 })
 export class ShopComponent {
   private readonly _shopService = inject(ShopService);
+  private readonly _activatedRoute = inject(ActivatedRoute);
 
   products = this._shopService.products;
   brands = this._shopService.brands;
   types = this._shopService.types;
+
+  brandIdSelected = toSignal(this._activatedRoute.queryParamMap.pipe(map(v => Number(v.get("brandId")))), { initialValue: 0 });
+  typeIdSelected = toSignal(this._activatedRoute.queryParamMap.pipe(map(v => Number(v.get("typeId")))), { initialValue: 0 });
 }
--- src/app/shop/shop.service.ts
+++ src/app/shop/shop.service.ts
@@ -1,12 +1,13 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable, inject, computed } from '@angular/core';
 import { toSignal } from '@angular/core/rxjs-interop';
 
 import { PaginatedResponse } from '../shared/models/paginated-response.model';
 import { Product } from '../product/models/product.model';
-import { catchError } from 'rxjs';
+import { catchError, shareReplay, switchMap } from 'rxjs';
 import { Brand } from '../shared/models/brand.model';
 import { Type } from '../shared/models/type.model';
+import { ActivatedRoute, ActivatedRouteSnapshot } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
@@ -15,9 +16,27 @@ export class ShopService {
   static BASE_URL = "https://localhost:7136/api";
 
   private _http = inject(HttpClient);
+  private _activatedRoute = inject(ActivatedRoute);
 
   // Products
-  private productsResult$ = this._http.get<PaginatedResponse<Product[]>>(ShopService.BASE_URL + "/products");
+  private productsResult$ = this._activatedRoute.queryParamMap.pipe(
+    shareReplay(),
+    switchMap(value => {
+      let params = new HttpParams();
+      const brandId = value.get("brandId");
+      const typeId = value.get("typeId");
+
+      if (brandId) {
+        params = params.append("brandId", brandId);
+      }
+
+      if (typeId) {
+        params = params.append("typeId", typeId);
+      }
+      console.log("gfg", value);
+      return this._http.get<PaginatedResponse<Product[]>>(ShopService.BASE_URL + "/products?pageSize=50", { params })
+    })
+  ) ;
   private brandsResult$ = this._http.get<Brand[]>(ShopService.BASE_URL + "/products/brands");
   private typesResult$ = this._http.get<Type[]>(ShopService.BASE_URL + "/products/types");
 

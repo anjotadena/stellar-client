3dd8e0f58c1963f6b1b39c675d31a25fff82e240
Anjo Tadena <tadena.anjo@gmail.com>
2023-12-06 13:53:08 +0800

    Add toastr

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,21 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 import { routes } from './app.routes';
 import { errorInterceptor } from './core/interceptors/error.interceptor';
+import { provideToastr } from 'ngx-toastr';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
+    provideAnimations(),
+    provideToastr({
+      timeOut: 10000,
+      positionClass: 'toast-top-right',
+      preventDuplicates: true,
+    }),
     provideHttpClient(withInterceptors([errorInterceptor])),
   ]
 };

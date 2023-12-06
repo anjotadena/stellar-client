79d48812006e56d72cde28e04bd46cba74e754d0
Anjo Tadena <tadena.anjo@gmail.com>
2023-12-06 20:29:16 +0800

    Add carousel

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { NgbCarouselModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'sc-home',
   standalone: true,
-  imports: [],
+  imports: [NgbCarouselModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,6 +2,7 @@ import { PaginationHeaderComponent } from './ui/pagination-header/pagination-hea
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SectionHeaderComponent } from './ui/section-header/section-header.component';
+import { NgbCarouselModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [],

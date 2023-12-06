2b8742f0676756390183e89bacb225b33e53ca0a
Anjo Tadena <tadena.anjo@gmail.com>
2023-12-06 08:19:57 +0800

    Add basic routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,13 +5,21 @@ import { RouterOutlet } from '@angular/router';
 import { CoreModule } from './core/core.module';
 import { SharedModule } from './shared/shared.module';
 import { ShopModule } from './shop/shop.module';
+import { HomeModule } from './home/home.module';
 
 @Component({
-    selector: 'app-root',
-    standalone: true,
-    templateUrl: './app.component.html',
-    styleUrl: './app.component.scss',
-    imports: [CommonModule, RouterOutlet, CoreModule, SharedModule, ShopModule]
+  selector: 'app-root',
+  standalone: true,
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    CoreModule,
+    SharedModule,
+    ShopModule,
+    HomeModule,
+  ],
 })
 export class AppComponent {
   title = 'Stellar';

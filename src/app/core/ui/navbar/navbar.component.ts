31f7810d4355bfcef8623e4b2b4378ae450a6d5d
Anjo Tadena <tadena.anjo@gmail.com>
2023-12-08 09:08:28 +0800

    Fix cart item count

--- src/app/core/ui/navbar/navbar.component.ts
+++ src/app/core/ui/navbar/navbar.component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { CartService } from '../../../cart/cart.service';
+import { CommonModule } from '@angular/common';
+import { CartItem } from '../../../shared/models/cart-item.model';
 
 @Component({
   selector: 'sc-navbar',
   standalone: true,
-  imports: [RouterModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss',
 })
-export class NavbarComponent {}
+export class NavbarComponent {
+  constructor(readonly _cartService: CartService) {}
+
+  getCount(items: CartItem[]) {
+    return items.reduce((sum, item) => sum + item.quantity, 0);
+  }
+}

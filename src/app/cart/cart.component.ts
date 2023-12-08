baedc0a6a090544c6809c997044020f83d7d77fc
Anjo Tadena <tadena.anjo@gmail.com>
2023-12-08 09:24:14 +0800

    Add cart page

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,22 +1,14 @@
 import { Component, Input } from '@angular/core';
-import { Product } from '../product/models/product.model';
 import { CartService } from './cart.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'sc-cart',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './cart.component.html',
   styleUrl: './cart.component.scss'
 })
 export class CartComponent {
-  @Input() product?: Product;
-
-  constructor(private readonly _cartService: CartService) { }
-
-  addItemToCart() {
-    if (this.product) {
-      this._cartService.addItemToCart(this.product);
-    }
-  }
+  constructor(public readonly _cartService: CartService) { }
 }
